06f06020cd9d5d3e2d43107b333aa591501400c9
DESKTOP-PLJM5AE\user <vhzkfl2451@naver.com>
2018-05-15 14:56:16 +0900

    GameManager, AudioManager 추가

---
+++ Assets/Frameworks/Code/Hyerim/AudioManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance = new AudioManager();
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+        DontDestroyOnLoad(this);
+    }
+
+    AudioSource bgm;
+    AudioSource efx;
+
+    public void PlayerSingle(AudioClip clip)
+    {
+        bgm.clip = clip;
+        bgm.Play();
+    }
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+}
---
+++ Assets/Frameworks/Code/Hyerim/GameManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = new GameManager();
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+        DontDestroyOnLoad(this);
+    }
+
+    public float playTime;
+    public int money;
+
+    public AudioClip openingBGM;
+
+    // Use this for initialization
+    void Start()
+    {
+        AudioManager.instance.PlayerSingle(openingBGM);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RunTime();
+    }
+
+    private void RunTime()
+    {
+        playTime += Time.deltaTime;
+    }
+
+    public void GameClear()
+    {
+
+    }
+
+    public void SettingWindow()
+    {
+
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
