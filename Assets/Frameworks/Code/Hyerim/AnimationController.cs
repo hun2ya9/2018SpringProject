eb2a65790dc90cbeb05b37a986bf828891f824c2
hun2ya9 <hun2ya9@naver.com>
2018-06-03 18:57:16 +0900

    플레이어 Animator 트랜지션 알맞게 수정하였고, 플레이어에게 AnimationController 부착시켜 애니메이션 테스트 가능함.

---
+++ Assets/Frameworks/Code/Hyerim/AnimationController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using Sirenix.OdinInspector;
+using System;
+
+public class AnimationController : MonoBehaviour
+{
+    public Animator playerAni;
+    public SpriteRenderer render;
+    private bool isFlip;
+
+    [Button(name : "Reset")]
+    public void StateReset()
+    {
+        playerAni.SetBool("isMove", false);
+        playerAni.SetBool("isSwing", false);
+    }
+    [Button(name : "LeftMove")]
+    public void LeftMove()
+    {
+        playerAni.SetBool("isMove", true);
+        if (isFlip)
+        {
+            isFlip = false;
+            render.flipX = isFlip;
+        }
+    }
+    [Button(name: "RightMove")]
+    public void RightMove()
+    {
+        playerAni.SetBool("isMove", true);
+        if (!isFlip)
+        {
+            isFlip = true;
+            render.flipX = isFlip;
+        }
+    }
+    [Button(name: "Swing")]
+    public void Swing()
+    {
+        playerAni.SetBool("isSwing", true);
+    }
+    [Button(name: "Hit")]
+    public void Hit()
+    {
+        playerAni.SetTrigger("Hit");
+    }
+    [Button(name: "Die")]
+    public void Die()
+    {
+        playerAni.SetTrigger("Die");
+    }
+
+}
