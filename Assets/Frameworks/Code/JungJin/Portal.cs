0d013f895d331ea4febdd7c5b1aee64ef470dee4
DESKTOP-GBR3NJB\User4579 <dlwjdwls6504@gmail.com>
2018-06-07 03:23:55 +0900

    포탈 기능 추가 / 사운드 적용 부탁.

---
+++ Assets/Frameworks/Code/JungJin/Portal.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour {
+
+    public GameObject ExitPortal;
+
+    void Start()
+    {
+        if(ExitPortal==null)
+        {
+            gameObject.SetActive(false);
+        }
+        
+    }
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            if(Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                GameObject visitor = col.gameObject;
+
+                StartCoroutine(TelePort(visitor));
+            }
+        }
+    }
+
+    IEnumerator TelePort(GameObject targetObject)
+    {
+        yield return new WaitForSeconds(1);
+        targetObject.transform.position = new Vector2(ExitPortal.transform.position.x, ExitPortal.transform.position.y);
+    }
+}
